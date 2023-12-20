4f0a8f548fda6dc461e2346b864e498edf9bdef0
ngophuhung96 <hungnp.1704@gmail.com>
2023-12-20 14:42:35 +0700

    add test

--- src/test/java/com/example/demo/RedisTest.java
+++ src/test/java/com/example/demo/RedisTest.java
@@ -4,44 +4,37 @@ import com.github.fppt.jedismock.RedisServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.redisson.Redisson;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@SpringBootTest
 public class RedisTest {
-
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
     private RedisServer redisServer;
-    private RedissonClient redissonClient;
 
     @BeforeEach
-    void beforeTest() throws Exception {
-        redisServer = RedisServer.newRedisServer();
+    void startRedis() throws Exception {
+        redisServer = RedisServer.newRedisServer(6379);
         redisServer.start();
-
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://" + this.redisServer.getHost() + ":" + this.redisServer.getBindPort());
-        redissonClient = Redisson.create(config);
     }
 
     @AfterEach
-    void afterTest() throws IOException {
+    void stopRedis() throws Exception {
         redisServer.stop();
     }
 
     @Test
-    public void testMethod() {
-        String key = "an-example-key";
-        RLock rLock = this.redissonClient.getLock(key);
-
-        rLock.lock();
-        assertTrue(rLock.isLocked());
-        rLock.unlock();
-        assertFalse(rLock.isLocked());
+    void test() {
+        String key = "testKey";
+        String value = "testValue";
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
+        String result = valueOperations.get(key);
+        assertEquals(value, result);
     }
 }
